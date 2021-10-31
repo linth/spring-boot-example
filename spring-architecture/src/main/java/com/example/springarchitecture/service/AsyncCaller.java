00824718937b41c90d432df44ae28f5080cf9b61
linth <zhua0404@gmail.com>
2021-10-31 22:26:15 +0800

    add Spring Async empty example.

---
+++ spring-architecture/src/main/java/com/example/springarchitecture/service/AsyncCaller.java
@@ -0,0 +1,13 @@
+package com.example.springarchitecture.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 
+ * Reference:
+ *  - https://ithelp.ithome.com.tw/articles/10278638
+ */
+@Service
+public class AsyncCaller {
+    
+}
