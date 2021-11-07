a5e43293a487a8a2f2f6866e1009f1a9c3a7c9e1
linth <zhua0404@gmail.com>
2021-11-07 18:04:48 +0800

    add comments.

--- spring-architecture/src/test/java/com/example/springarchitecture/controller/RequestMappingExampleControllerTest.java
+++ spring-architecture/src/test/java/com/example/springarchitecture/controller/RequestMappingExampleControllerTest.java
@@ -6,6 +6,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 /**
+ * 測試CRUD RESTful整個過程是否有如預期一樣運作，Java 提供了一個In-memory的測試資料庫H2
+ * ，若我們今天是介接MySQL這樣要執行整合測試就顯得困難許多，
+ * 必須安裝並運行MySQL保持連接且要設置正確得用戶與資料庫 。
+ * H2 Database就是一個很好的測試工具，由於資料是內存，測試程序結束後資料會不留痕跡地被刪除，
+ * 以完全獨立的方式運行。
+ * 
+ * 
  * 
  * Reference:
  *  - https://matthung0807.blogspot.com/2021/04/spring-mvc-requestparam-url.html
@@ -13,6 +20,7 @@ import org.springframework.test.web.servlet.MockMvc;
  *  - https://chikuwa-tech-study.blogspot.com/2021/05/spring-boot-mockmvc-integration-test-2.html
  *  - https://ithelp.ithome.com.tw/articles/10281055
  *  - https://matthung0807.blogspot.com/2021/01/spring-web-requestmapping-optional-pathvariable-api.html
+ *  - https://ithelp.ithome.com.tw/articles/10246525
  */
 @AutoConfigureMockMvc
 @SpringBootTest
