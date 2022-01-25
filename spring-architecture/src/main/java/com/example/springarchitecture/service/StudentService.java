b5a9e7373688412a2b31e52dbdcd6b962dab5fd0
linth <zhua0404@gmail.com>
2022-01-25 15:11:56 +0800

    add repository and mongoTemplate way to create a new Student object.

---
+++ spring-architecture/src/main/java/com/example/springarchitecture/service/StudentService.java
@@ -0,0 +1,34 @@
+package com.example.springarchitecture.service;
+
+import com.example.springarchitecture.Entity.Student;
+import com.example.springarchitecture.repository.StudentRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class StudentService {
+    // 使用 repository 來存取資料
+    @Autowired
+    private StudentRepository repository;
+
+    public void createStudentByRepository(Student student) {
+        log.info("call createStudentByRepository().");
+        repository.save(student);
+        log.info("finish createStudentByRepository().");
+    }
+
+    // 使用 MongoTemplate 來存取資料
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public void createStudentByMongoTemplate(Student student) {
+        log.info("call createStudentByMongoTemplate().");
+        mongoTemplate.save(student);
+        log.info("finish createStudentByMongoTemplate().");
+    }
+}
